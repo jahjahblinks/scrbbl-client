f3dbf9f802ee7aa904471bde96835a21f75848c5
lsssmmns <97260713+lsssmmns@users.noreply.github.com>
2022-03-05 19:56:44 -0800

    Update Room.vue

--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -178,7 +178,7 @@ script: [
                 <input
                   id="message_typespace"
                   value="test"
-                  v-model="message"
+                  //v-model="message"
                   class="input is-borderless"
                   type="text"
                   placeholder="Send a message..."
