594196bc2a0ca1a6c5a83db44ff7f145b105edd3
Brendan-Towell <towellb14@gmail.com>
2022-04-28 11:33:17 -0700

    Update WhiteBoard.vue

--- client/src/components/WhiteBoard.vue
+++ client/src/components/WhiteBoard.vue
@@ -49,6 +49,7 @@
 import { Hands, HAND_CONNECTIONS } from "@mediapipe/hands";
 import { Camera } from "@mediapipe/camera_utils";
 import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+console.log("imports");
 
 export default {
   name: "Whiteboard",
