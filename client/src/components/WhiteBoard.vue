f19d4eecef1e87aae90f7e64db953aadcf4a4720
Brendan-Towell <towellb14@gmail.com>
2022-05-05 15:06:25 -0700

    bugfix

--- client/src/components/WhiteBoard.vue
+++ client/src/components/WhiteBoard.vue
@@ -119,13 +119,13 @@ export default {
     onResults(results) {
       var canvas = document.getElementsByClassName("overlay");
       var ctx = canvas.getContext("2d");
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       if (results.multiHandLandmarks) {
         for (const landmarks of results.multiHandLandmarks) {
           let scaledPos = { x: 800 - parseInt(1600*(landmarks[8].x - 0.25), 10), y: parseInt(1200*(landmarks[8].y-0.25), 10)};  
-          this.ctx.beginPath();
-          this.ctx.arc(scaledPos.x, scaledPos.y, 25, 0, 2 * Math.PI);
-          this.ctx.stroke();
+          ctx.beginPath();
+          ctx.arc(scaledPos.x, scaledPos.y, 25, 0, 2 * Math.PI);
+          ctx.stroke();
           if(landmarks[8].x > 0.75 || landmarks[8].x < 0.25 || landmarks[8].y > 0.75 || landmarks[8].y < 0.25){
             this.prevPos.x = null;
             this.prevPos.y = null;
