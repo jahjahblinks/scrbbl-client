12ed395cdc26596db971c505def95936d156d929
lsssmmns <97260713+lsssmmns@users.noreply.github.com>
2022-05-12 16:28:01 -0700

    Update Tutorial.vue

--- client/src/views/Tutorial.vue
+++ client/src/views/Tutorial.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container" style="height:100%;width:100%">
+  <div class="container">
     <section class="hero">
       <div class="hero-body room-header">
         <div class="container">
@@ -11,7 +11,7 @@
       </div>
     </section>
     <section class="section">
-      <iframe src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"> width="1200" height="800"</iframe>
+      <iframe  style="height:5rem;width:9rem" class="hero is-fullwidth" src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
     </section>
   </div>
 </template>
