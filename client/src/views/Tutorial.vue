db31270aaa575d6f0a41965195b85e06a9674b23
alexander-tan <lxndrtn@gmail.com>
2022-05-12 15:25:40 -0700

    update tutorial page

--- client/src/views/Tutorial.vue
+++ client/src/views/Tutorial.vue
@@ -1,11 +1,11 @@
 <template>
 
 <div class="section">
-    <div class="hero-body room-header">
+    <div class="hero-body room-header" >
         <div class="container">
             <h1 class="title room">Tutorial</h1>
         </div>
     </div>
-    <iframe src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
+    <iframe style="height:300px; width:700px" src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
   </div>
 </template>
