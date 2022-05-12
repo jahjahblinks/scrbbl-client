d8961fd56b0e95b536ea3d6e87b4c6a65463ff05
alexander-tan <lxndrtn@gmail.com>
2022-05-12 15:46:55 -0700

    asdkjasd

--- client/src/views/Tutorial.vue
+++ client/src/views/Tutorial.vue
@@ -9,8 +9,12 @@
     <iframe style="height:300px; width:700px" src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
 </section> -->
 
-<section class="container">
-    <iframe src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
-</section>
 
+<section class="hero is-danger is-fullheight">
+    <div class="hero-body">
+      <div class="">
+        <iframe src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
+      </div>
+    </div>
+  </section>
 </template>
