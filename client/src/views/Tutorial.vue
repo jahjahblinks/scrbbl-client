d7435336d20cf11444a5ed5ec25b82f6e6423be1
alexander-tan <lxndrtn@gmail.com>
2022-05-12 15:44:04 -0700

    sdfsadf

--- client/src/views/Tutorial.vue
+++ client/src/views/Tutorial.vue
@@ -9,7 +9,7 @@
     <iframe style="height:300px; width:700px" src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
 </section> -->
 
-<section class="hero is-large">
+<section class="container">
     <iframe src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
 </section>
 
