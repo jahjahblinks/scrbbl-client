425e8183a4bb055f6a3f3a0db8e80c4f277549bd
alexander-tan <lxndrtn@gmail.com>
2022-05-12 14:45:48 -0700

    update tutorial page

--- client/src/views/Tutorial.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <iframe src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
-</template>
