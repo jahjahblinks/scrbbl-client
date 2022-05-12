5e75905e35f8a4ff868484dc5f3a8118b8d25e7f
alexander-tan <lxndrtn@gmail.com>
2022-05-12 16:25:20 -0700

    size

--- client/src/views/Tutorial.vue
+++ client/src/views/Tutorial.vue
@@ -11,7 +11,7 @@
       </div>
     </section>
     <section class="section">
-      <iframe class="hero is-fullwidth" src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"></iframe>
+      <iframe src="https://docs.google.com/document/d/e/2PACX-1vTD8WiNV5gDQHIPw5IKnd9AXqcw1xUFODQWVi1V5o4OasCsYNLPy2HvzrxTqy5iNe0UkWG8A3l9Yfdj/pub?embedded=true"> width="1200" height="800"</iframe>
     </section>
   </div>
 </template>
